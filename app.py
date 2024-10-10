e4c5430a2af1250c252972f0cd35b33a52b6a4c7
cm <111703202+CamCove29@users.noreply.github.com>
2024-10-10 15:16:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host="3.85.121.200",
+        host="98.81.251.98",
         database="db_clients",
         user="root",
         password="utec",
@@ -18,32 +18,19 @@ def get_db_connection():
     )
     return conn
 
-# Modelo para los datos de un cliente
-class Cliente(BaseModel):
-    ID_cliente: int  # Campo obligatorio
-    Nombre: str
-    Apellido: str
-    Fecha_nacimiento: str  # Utiliza formato ISO para fechas (YYYY-MM-DD)
-    Email: str
-    Telefono: str  # Campo obligatorio
-    Direccion: str  # Campo obligatorio
-    Fecha_registro: datetime  # Campo obligatorio
+class Usuario(BaseModel):
+    ID: int  # Opcional si es autogenerado
+    nombre_usuario: str
+    correo_electronico: str
+    contraseña: str
 
-# Modelo para el historial de un cliente
-class Historial(BaseModel):
-    ID_historial: int  # Campo obligatorio
-    ID_cliente: int  # Campo obligatorio
-    ID_libro: int  # Campo obligatorio
-    Fecha_lectura: datetime  # Campo obligatorio, generado automáticamente
-
-# Modelo para las valoraciones de un cliente
-class Valoracion(BaseModel):
-    ID_valoracion: int  # Campo obligatorio
-    ID_libro: int  # Campo obligatorio
-    ID_cliente: int  # Campo obligatorio
-    Puntuacion: int  # Asegúrate de que esté entre 1 y 5
-    Comentario: str  # Campo obligatorio
-    Fecha_valoracion: datetime  # Campo obligatorio, generado automáticamente
+class DatosUsuario(BaseModel):
+    ID_usuario: int  # Referencia al ID del usuario
+    nombres: str
+    apellidos: str
+    numero_telefono: str
+    edad: int
+    fecha_nacimiento: date
 
 
 # Get echo test for load balancer's health check
@@ -52,161 +39,127 @@ def get_echo_test():
     return {"message": "Echo Test OK"}
 
 
-# Obtener todos los clientes
-@app.get("/clientes")
-def get_clientes():
+# Obtener todos los usuarios
+@app.get("/usuarios")
+def get_usuarios():
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM Clientes")
-    clientes = cursor.fetchall()
+    cursor.execute("SELECT * FROM Usuarios")
+    usuarios = cursor.fetchall()
     cursor.close()
     conn.close()
-    return JSONResponse(content=clientes)
+    return JSONResponse(content=usuarios)
 
-
-# Obtener un cliente por su ID
-@app.get("/clientes/{id_cliente}")
-def get_cliente(id_cliente: int):
+# Obtener un usuario por su ID
+@app.get("/usuarios/{user_id}")
+def get_usuario(user_id: int):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM Clientes WHERE ID_cliente = %s", (id_cliente,))
-    cliente = cursor.fetchone()
+    cursor.execute("SELECT * FROM Usuarios WHERE ID = %s", (user_id,))
+    usuario = cursor.fetchone()
     cursor.close()
     conn.close()
+    if usuario:
+        return JSONResponse(content=usuario)
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
-    if cliente:
-        return JSONResponse(content=cliente)
-    raise HTTPException(status_code=404, detail="Cliente no encontrado")
-
-
-# Crear un nuevo cliente
-@app.post("/clientes", response_model=dict)
-def create_cliente(cliente: Cliente):
+# Crear un nuevo usuario
+@app.post("/usuarios")
+def create_usuario(usuario: Usuario):
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO Clientes (Nombre, Apellido, Email, Telefono, Direccion, Fecha_nacimiento, Fecha_registro)
-        VALUES (%s, %s, %s, %s, %s, %s, NOW())
-        RETURNING ID_cliente;
-    """, (
-    cliente.Nombre, cliente.Apellido, cliente.Email, cliente.Telefono, cliente.Direccion, cliente.Fecha_nacimiento))
-    cliente_id = cursor.fetchone()[0]
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {"ID_cliente": cliente_id, "message": "Cliente creado exitosamente"}
-
-
-# Actualizar un cliente existente
-@app.put("/clientes/{id_cliente}", response_model=dict)
-def update_cliente(id_cliente: int, cliente: Cliente):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        UPDATE Clientes 
-        SET Nombre = %s, Apellido = %s, Email = %s, Telefono = %s, Direccion = %s, Fecha_nacimiento = %s 
-        WHERE ID_cliente = %s
-    """, (
-    cliente.Nombre, cliente.Apellido, cliente.Email, cliente.Telefono, cliente.Direccion, cliente.Fecha_nacimiento,
-    id_cliente))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {"message": "Cliente actualizado exitosamente"}
-
-
-# Eliminar un cliente
-@app.delete("/clientes/{id_cliente}", response_model=dict)
-def delete_cliente(id_cliente: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM Clientes WHERE ID_cliente = %s", (id_cliente,))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {"message": "Cliente eliminado exitosamente"}
-
-
-# URL base de la API de libros
-LIBROS_API_URL = "http://3.85.121.200:8005"
-
-
-# Obtener historial de un cliente
-@app.get("/clientes/{id_cliente}/historial")
-def get_historial_cliente(id_cliente: int):
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("""
-        SELECT h.ID_historial, h.ID_libro, l.Titulo, h.Fecha_reserva 
-        FROM Historial_cliente h 
-        JOIN Libros l ON h.ID_libro = l.ID_libro 
-        WHERE h.ID_cliente = %s
-    """, (id_cliente,))
-    historial = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return JSONResponse(content=historial)
-
-
-# Agregar a historial
-@app.post("/clientes/{id_cliente}/historial")
-async def add_to_historial(id_cliente: int, historial: Historial):
-    # Llamada a la API de libros para obtener el ID_libro
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{LIBROS_API_URL}/libros/{historial.ID_libro}")
-        if response.status_code != 200:
-            raise HTTPException(status_code=404, detail="Libro no encontrado en la API de libros")
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO Historial_cliente (ID_cliente, ID_libro, Fecha_reserva)
-        VALUES (%s, %s, NOW())
-    """, (id_cliente, historial.ID_libro))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return {"message": "Libro agregado al historial exitosamente"}
-
-
-# Obtener valoraciones de un cliente
-@app.get("/clientes/{id_cliente}/valoraciones")
-def get_valoraciones_cliente(id_cliente: int):
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("""
-        SELECT v.ID_valoracion, l.Titulo, v.Puntuacion 
-        FROM Valoraciones v 
-        JOIN Libros l ON v.ID_libro = l.ID_libro 
-        WHERE v.ID_cliente = %s
-    """, (id_cliente,))
-    valoraciones = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return JSONResponse(content=valoraciones)
-
-
-# Crear una nueva valoración
-@app.post("/clientes/{id_cliente}/valoraciones")
-async def create_valoracion_cliente(id_cliente: int, valoracion: Valoracion):
-    # Llamada a la API de libros para obtener el ID_libro
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{LIBROS_API_URL}/libros/{valoracion.ID_libro}")
-        if response.status_code != 200:
-            raise HTTPException(status_code=404, detail="Libro no encontrado en la API de libros")
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO Valoraciones (ID_cliente, ID_libro, Puntuacion)
+        INSERT INTO Usuarios (nombre_usuario, correo_electronico, contraseña)
         VALUES (%s, %s, %s)
-    """, (id_cliente, valoracion.ID_libro, valoracion.Puntuacion))
+        RETURNING ID;
+    """, (usuario.nombre_usuario, usuario.correo_electronico, usuario.contraseña))
+    user_id = cursor.fetchone()[0]
     conn.commit()
     cursor.close()
     conn.close()
+    return {"ID": user_id, "message": "Usuario creado exitosamente"}
 
-    return {"message": "Valoración creada exitosamente"}
+# Actualizar un usuario
+@app.put("/usuarios/{user_id}")
+def update_usuario(user_id: int, usuario: Usuario):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE Usuarios 
+        SET nombre_usuario = %s, correo_electronico = %s, contraseña = %s 
+        WHERE ID = %s
+    """, (usuario.nombre_usuario, usuario.correo_electronico, usuario.contraseña, user_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Usuario actualizado exitosamente"}
+
+# Eliminar un usuario
+@app.delete("/usuarios/{user_id}")
+def delete_usuario(user_id: int):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Usuarios WHERE ID = %s", (user_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Usuario eliminado exitosamente"}
+
+# CRUD para la tabla Datos_Usuario
+
+# Obtener datos del usuario
+@app.get("/usuarios/{user_id}/datos")
+def get_datos_usuario(user_id: int):
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM Datos_Usuario WHERE ID_usuario = %s", (user_id,))
+    datos = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if datos:
+        return JSONResponse(content=datos)
+    raise HTTPException(status_code=404, detail="Datos del usuario no encontrados")
+
+# Crear datos de usuario
+@app.post("/usuarios/{user_id}/datos")
+def create_datos_usuario(user_id: int, datos_usuario: DatosUsuario):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO Datos_Usuario (ID_usuario, nombres, apellidos, numero_telefono, edad, fecha_nacimiento)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (
+        user_id, datos_usuario.nombres, datos_usuario.apellidos, 
+        datos_usuario.numero_telefono, datos_usuario.edad, datos_usuario.fecha_nacimiento))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Datos del usuario creados exitosamente"}
+
+# Actualizar datos de usuario
+@app.put("/usuarios/{user_id}/datos")
+def update_datos_usuario(user_id: int, datos_usuario: DatosUsuario):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE Datos_Usuario 
+        SET nombres = %s, apellidos = %s, numero_telefono = %s, edad = %s, fecha_nacimiento = %s
+        WHERE ID_usuario = %s
+    """, (
+        datos_usuario.nombres, datos_usuario.apellidos, datos_usuario.numero_telefono, 
+        datos_usuario.edad, datos_usuario.fecha_nacimiento, user_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Datos del usuario actualizados exitosamente"}
+
+# Eliminar datos de usuario
+@app.delete("/usuarios/{user_id}/datos")
+def delete_datos_usuario(user_id: int):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Datos_Usuario WHERE ID_usuario = %s", (user_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Datos del usuario eliminados exitosamente"}
